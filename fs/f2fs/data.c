20524abe38a3e0df371e4831d73e2794145755f5
stic-server-open <1138705738@qq.com>
2021-10-29 18:22:37 +0800

    fs: ext4-f2fs: Delete the Android I/O Trace that has never been used
    * These should be required for debugging in Android Studio
    * If you are an Android developer involved in debugging,
      please revert this submission.
    
    * Aquarius223: Repair and restore submission conflicts
    * This reverts commit ba81f70 a989b60 b894d59 5089c1e a533ec7
    
    * Aquarius223: Changes Linux 3.18.y merge f2fs-stable kernel
    
    Change-Id: Ib77054fd3f3bb7aa30dd0dd750d7ca0b79e2deb4

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -24,7 +24,6 @@
 #include "node.h"
 #include "segment.h"
 #include <trace/events/f2fs.h>
-#include <trace/events/android_fs.h>
 
 #define NUM_PREALLOC_POST_READ_CTXS	128
 
@@ -2478,16 +2477,6 @@ static int f2fs_write_begin(struct file *file, struct address_space *mapping,
 	block_t blkaddr = NULL_ADDR;
 	int err = 0;
 
-	if (trace_android_fs_datawrite_start_enabled()) {
-		char *path, pathbuf[MAX_TRACE_PATHBUF_LEN];
-
-		path = android_fstrace_get_pathname(pathbuf,
-						    MAX_TRACE_PATHBUF_LEN,
-						    inode);
-		trace_android_fs_datawrite_start(inode, pos, len,
-						 current->pid, path,
-						 current->comm);
-	}
 	trace_f2fs_write_begin(inode, pos, len, flags);
 
 	err = f2fs_is_checkpoint_ready(sbi);
@@ -2593,7 +2582,6 @@ static int f2fs_write_end(struct file *file,
 {
 	struct inode *inode = page->mapping->host;
 
-	trace_android_fs_datawrite_end(inode, pos, len);
 	trace_f2fs_write_end(inode, pos, len, copied);
 
 	/*
@@ -2658,27 +2646,6 @@ static ssize_t f2fs_direct_IO(int rw, struct kiocb *iocb, struct iov_iter *iter,
 	if (f2fs_force_buffered_io(inode, rw))
 		return 0;
 
-	if (trace_android_fs_dataread_start_enabled() && (rw == READ)) {
-		char *path, pathbuf[MAX_TRACE_PATHBUF_LEN];
-
-		path = android_fstrace_get_pathname(pathbuf,
-						    MAX_TRACE_PATHBUF_LEN,
-						    inode);
-		trace_android_fs_dataread_start(inode, offset,
-						count, current->pid, path,
-						current->comm);
-	}
-	if (trace_android_fs_datawrite_start_enabled() && (rw == WRITE)) {
-		char *path, pathbuf[MAX_TRACE_PATHBUF_LEN];
-
-		path = android_fstrace_get_pathname(pathbuf,
-						    MAX_TRACE_PATHBUF_LEN,
-						    inode);
-		trace_android_fs_datawrite_start(inode, offset, count,
-						 current->pid, path,
-						 current->comm);
-	}
-
 	trace_f2fs_direct_IO_enter(inode, offset, count, rw);
 
 	/* if (rw == WRITE && whint_mode == WHINT_MODE_OFF)
@@ -2701,11 +2668,6 @@ static ssize_t f2fs_direct_IO(int rw, struct kiocb *iocb, struct iov_iter *iter,
 	}
 
 	trace_f2fs_direct_IO_exit(inode, offset, count, rw, err);
-
-	if (trace_android_fs_dataread_start_enabled() && (rw == READ))
-		trace_android_fs_dataread_end(inode, offset, count);
-	if (trace_android_fs_datawrite_start_enabled() && (rw == WRITE))
-		trace_android_fs_datawrite_end(inode, offset, count);
 
 	return err;
 }
