05628335fbb192b106e2ac1b5f906e66b91bb4e6
Sahitya Tummala <stummala@codeaurora.org>
2022-01-27 17:56:46 +0800

    fs: f2fs: change to use rwsem for cp_mutex
    Use rwsem to ensure serialization of the callers and to avoid
    starvation of high priority tasks, when the system is under
    heavy IO workload.
    
    * Aquarius223: Backport Linux 3.18.y fs/f2fs
    
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Change-Id: I03f3bae9e336d545992791c6da3134b8a9175624

--- fs/f2fs/gc.c
+++ fs/f2fs/gc.c
@@ -1713,7 +1713,7 @@ out_unlock:
 
 	freeze_super(sbi->sb);
 	down_write(&sbi->gc_lock);
-	mutex_lock(&sbi->cp_mutex);
+	down_write(&sbi->cp_global_sem);
 
 	spin_lock(&sbi->stat_lock);
 	if (shrunk_blocks + valid_user_blocks(sbi) +
@@ -1757,7 +1757,7 @@ recover_out:
 		spin_unlock(&sbi->stat_lock);
 	}
 out_err:
-	mutex_unlock(&sbi->cp_mutex);
+	up_write(&sbi->cp_global_sem);
 	up_write(&sbi->gc_lock);
 	thaw_super(sbi->sb);
 	clear_sbi_flag(sbi, SBI_IS_RESIZEFS);
